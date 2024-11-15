66f3ebd0ceb14cbb5fb8944635ff02590aa91cae
Beyond Understanding <127466825+b3yund@users.noreply.github.com>
2024-11-15 03:14:14 -0500

    Updated commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/celestialcenter">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
